8e3e9823e7c88bfeadc103b9005d1f8efc6067b5
1stfaccia1 <chad.m.wittman75@gmail.com>
2018-10-28 07:08:54 -0500

    idk

--- js/mian.js
+++ js/mian.js
@@ -1,6 +1,6 @@
-const particles = require('particles-js');
+var particlesJS = 'particles-js';
 
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'assets/particles.json', function() {
+particlesJS.load('particles-js', 'particles.json', function() {
   console.log('callback - particles.js config loaded');
 })
